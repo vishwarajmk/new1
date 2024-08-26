c7a1707ac1ecd6edb4d048f962af7b62ed66250f
vishwarajmk <164055622+vishwarajmk@users.noreply.github.com>
2024-08-26 16:00:15 +0530

    Create text4.js

---
+++ text4.js
@@ -0,0 +1,14 @@
+
+{
+  "name": "my-app",
+  "version": "1.0.0",
+  "description": "A simple Node.js web application",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2"
+  }
+}
